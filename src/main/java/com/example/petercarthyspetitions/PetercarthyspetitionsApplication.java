ec7632b68e5d4c9871fcddb9c8899c5b24f8437c
CarthyP <petercarthy@live.com>
2023-11-22 22:51:47 +0000

    Debugging POM File

--- src/main/java/com/example/petercarthyspetitions/PetercarthyspetitionsApplication.java
+++ src/main/java/com/example/petercarthyspetitions/PetercarthyspetitionsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeterCarthysPetitionsApplication {
+public class PetercarthyspetitionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeterCarthysPetitionsApplication.class, args);
+		SpringApplication.run(PetercarthyspetitionsApplication.class, args);
 	}
 
 }
